2febe20b453f40a8ca8e9ba78c157914671ee40c
Eds-Dbug <edsDbug@gmail.com>
2022-09-20 04:35:52 +0000

    finish excersises

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,11 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum('annual_revenue')
+puts "the total revenue is #{total} with #{Store.count} stores"
+
+avg = Store.average('annual_revenue')
+puts "the average revenue is #{avg} with #{Store.count} stores"
+
+millionStores = Store.where('annual_revenue >= 1000000').count
+puts "Number of million dollar stores is: #{millionStores}"
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,10 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurrama", last_name: "Bijuu", hourly_rate: 60)
+@store1.employees.create(first_name: "Obito", last_name: "Uzamaki", hourly_rate: 60)
+
+@store2.employees.create(first_name: "Tsunade", last_name: "Senju", hourly_rate: 60)
+@store2.employees.create(first_name: "Hashirama", last_name: "Senju", hourly_rate: 100)
+@store2.employees.create(first_name: "Tobirama", last_name: "Senju", hourly_rate: 60)
